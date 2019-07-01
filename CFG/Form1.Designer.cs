6d6bf7deae1691fe5febb01bfc7b65c591052f2d
Azimi_Yusef Abad <azimi85@yahoo.com>
2019-07-01 22:22:51 +0430

    Add project files.

---
+++ CFG/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace CFG
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.label10 = new System.Windows.Forms.Label();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(30, 89);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(50, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "orgURL :";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(30, 115);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(56, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "orgName :";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(30, 141);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(65, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "orgShahab :";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(30, 167);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(73, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "personName :";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(30, 193);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(74, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "personFamily :";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(30, 219);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(82, 13);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "personShahab :";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(30, 245);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(70, 13);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "personEmail :";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(119, 86);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(250, 20);
+			this.textBox1.TabIndex = 7;
+			this.textBox1.Text = "www.rgtamin.ir";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(119, 112);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(250, 20);
+			this.textBox2.TabIndex = 8;
+			this.textBox2.Text = "رفاه گستر تامین اجتماعی";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(119, 138);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(250, 20);
+			this.textBox3.TabIndex = 9;
+			this.textBox3.Text = "2000010101775377";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(119, 164);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(250, 20);
+			this.textBox4.TabIndex = 10;
+			this.textBox4.Text = "رضا";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(119, 190);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(250, 20);
+			this.textBox5.TabIndex = 11;
+			this.textBox5.Text = "خادم";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(119, 216);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(250, 20);
+			this.textBox6.TabIndex = 12;
+			this.textBox6.Text = "1000000349017611";
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(119, 242);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(250, 20);
+			this.textBox7.TabIndex = 13;
+			this.textBox7.Text = "rkhadem@rgtamin.ir";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(119, 268);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(249, 23);
+			this.button1.TabIndex = 14;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(21, 22);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(92, 13);
+			this.label8.TabIndex = 16;
+			this.label8.Text = "مسیر ذخیره فایل :";
+			// 
+			// textBox9
+			// 
+			this.textBox9.Font = new System.Drawing.Font("Courier New", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox9.Location = new System.Drawing.Point(33, 309);
+			this.textBox9.Multiline = true;
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox9.Size = new System.Drawing.Size(545, 398);
+			this.textBox9.TabIndex = 17;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(200, 22);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(69, 13);
+			this.label9.TabIndex = 18;
+			this.label9.Text = "D:\\2\\aaa.cfg";
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(119, 17);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 22);
+			this.button2.TabIndex = 19;
+			this.button2.Text = "جستجو";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label10.Location = new System.Drawing.Point(6, 52);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(107, 13);
+			this.label10.TabIndex = 16;
+			this.label10.Text = "مسیر فایل openSSL :";
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openssl.exe";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(119, 47);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 20;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(614, 739);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ CFG/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CFG
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			folderBrowserDialog1.ShowDialog();
+			string pathAddress = folderBrowserDialog1.SelectedPath.ToString();
+			label9.Text = $"{pathAddress}\\sysBankers.cfg";
+		}
+
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "|openssl.exe";
+			dlg.FileName = "openssl.exe";
+
+
+
+			dlg.ShowDialog();
+			//openFileDialog1.ShowDialog();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrEmpty(textBox1.Text) ||
+				string.IsNullOrEmpty(textBox2.Text) ||
+				string.IsNullOrEmpty(textBox3.Text) ||
+				string.IsNullOrEmpty(textBox4.Text) ||
+				string.IsNullOrEmpty(textBox5.Text) ||
+				string.IsNullOrEmpty(textBox6.Text) ||
+				string.IsNullOrEmpty(textBox7.Text)
+				)
+			{
+				MessageBox.Show("همه ی فیلد ها اجباری میباشند");
+
+				return;
+			}
+
+			System.IO.StreamWriter stream = null;
+
+
+			try
+			{
+				Encoding utf8WithoutBom = new UTF8Encoding(false);
+
+				string pathAddress = label9.Text;
+
+				stream = new System.IO.StreamWriter	(path: pathAddress, append: true, encoding: utf8WithoutBom);
+
+				string t1 = textBox1.Text;
+				string t2 = textBox2.Text;
+				string t3 = textBox3.Text;
+				string t4 = textBox4.Text;
+				string t5 = textBox5.Text;
+				string t6 = textBox6.Text;
+				string t7 = textBox7.Text;
+
+
+				StringBuilder sw = new StringBuilder();
+				sw.Append($"###############################################{Environment.NewLine}");
+				sw.Append($"## CUSTOMERS SYSTEM CERTIFICATE SUBJECT INFO ##{Environment.NewLine}");
+				sw.Append($"###############################################{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"orgURL = {t1} # Domain's IP or URL{Environment.NewLine}");
+				sw.Append($"orgName ={t2}{Environment.NewLine}");
+				sw.Append($"orgShahab = {t3} # Organization SHAHAB ID – 16 digits{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"personName = {t4} # System Admin. Name in Farsi{Environment.NewLine}");
+				sw.Append($"personFamily = {t5} # System Admin. Family (surname) in Farsi{Environment.NewLine}");
+				sw.Append($"personShahab = {t6} # System Admin. SHAHAB ID - 16 digits{Environment.NewLine}");
+				sw.Append($"personEmail = {t7} # System Admin. Email Address{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"###############################{Environment.NewLine}");
+				sw.Append($"## DO NOT CHANGE LINES BELOW ##{Environment.NewLine}");
+				sw.Append($"###############################{Environment.NewLine}");
+				sw.Append($"[ req ]{Environment.NewLine}");
+				sw.Append($"default_bits = 2048{Environment.NewLine}");
+				sw.Append($"default_md = sha256{Environment.NewLine}");
+				sw.Append($"prompt = no{Environment.NewLine}");
+				sw.Append($"distinguished_name = req_distinguished_name{Environment.NewLine}");
+				sw.Append($"req_extensions = req_ext{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"[ req_distinguished_name ]{Environment.NewLine}");
+				sw.Append($"countryName = \"ir\"{Environment.NewLine}");
+				sw.Append($"organizationName = $ENV::orgName-$ENV::orgShahab{Environment.NewLine}");
+				sw.Append($"commonName = $ENV::orgURL{Environment.NewLine}");
+				sw.Append($"emailAddress = $ENV::personEmail{Environment.NewLine}");
+				sw.Append($"givenName = $ENV::personName{Environment.NewLine}");
+				sw.Append($"surname = $ENV::personFamily{Environment.NewLine}");
+				sw.Append($"serialNumber = $ENV::personShahab{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"[ req_ext ]{Environment.NewLine}");
+				sw.Append($"subjectAltName = @alt_names{Environment.NewLine}");
+				sw.Append($"{Environment.NewLine}");
+				sw.Append($"[ alt_names ]{Environment.NewLine}");
+				sw.Append($"DNS.1 = $ENV::orgURL");
+
+
+				textBox9.Text = sw.ToString();
+				stream.Write(sw.ToString());
+
+				//stream.Write(Environment.NewLine);
+
+				stream.Close();
+
+				MessageBox.Show("فایل متنی با موفقیت ایجاد یا ذخیره گردید ...");
+
+			}
+			catch (System.Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				if (stream != null)
+				{
+					stream.Dispose();
+					stream = null;
+				}
+			}
+		}
+
+
+	}
+}
